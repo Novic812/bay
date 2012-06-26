6dbdb94ef368a806baa816e0f05a6ec25fd9b917
Steven Penny <svnpenn@gmail.com>
2012-06-26 06:53:16 -0500

    M  bootstrap.sh R  firefox/userContent.css -> firefox/chrome/userContent.css

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,13 @@
-#!/bin/sh
-# Cygwin post install
-d="C:/home/GitHub/dotfiles"
+#!/bin/bash
+
+# ~ files
+cd "C:/home/GitHub/dotfiles"
+cp -r .bash_profile .bash_prompt .gemrc .gitconfig .inputrc .ssh .wgetrc ~
+
+# Firefox files
+cd "firefox"
+eval "cp -r . $(cygpath -F26)/*/*/Profiles/*"
 
 # Mount C:
 mount -c "/"
 mount -m > "/etc/fstab"
-
-# Create symlinks
-#ls -A "$d" | while read line; do
-#  ln -sf "$d/$line"
-#done
-ln -sf "$d/.bash_profile"
-ln -sf "$d/.gemrc"
-ln -sf "$d/.gitconfig"
-ln -sf "$d/.inputrc"
-ln -sf "$d/.ssh"
-ln -sf "$d/.wgetrc"
-ln -sf "$d/bin"
-
-# Create Firefox symlinks
-base="/c/home/GitHub/dotfiles/firefox"
-profile="$(cygpath -F26)/*/*/Profiles/*"
-
-mklink(){
-    test -d "$2" && local d=/d
-    cmd /c mklink $d "$(cygpath -w $1)" "$(cygpath -w $2)"
-}
-
-# Custom preferences
-cd $profile
-mklink "user.js" "$base/user.js"
-
-# Custom CSS
-mklink -d "chrome" "$base/chrome"
