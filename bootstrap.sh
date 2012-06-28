2b9e2471a99679a2466a86edd17e010496fa6e99
Steven Penny <svnpenn@gmail.com>
2012-06-28 02:40:09 -0500

    M  bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+mklink(){
+  mapfile -t a < <(cygpath -w "$@")
+  [ -d "$2" ] && d=/d || d=
+  cmd /c mklink $d "${a[@]}"
+}
+
 # ~ files
-cd "C:/home/GitHub/dotfiles"
-cp -r .bash_profile .bash_prompt .gemrc .gitconfig .inputrc .ssh .wgetrc ~
+for i in .bash_profile .bash_prompt .gemrc .gitconfig .inputrc .ssh .wgetrc; do
+  mklink "$i" "/c/home/GitHub/dotfiles/$i"
+done
+
+# Symlink all Firefox profiles
+for i in "$APPDATA"/*/*/Profiles/*; do
+  cd "$i"
+  mklink "user.js" "/c/home/GitHub/dotfiles/firefox/user.js"
+  mklink "chrome" "/c/home/GitHub/dotfiles/firefox/chrome"
+done
 
 # Mount C:
 mount -c "/"
 mount -m > "/etc/fstab"
-
-# Symlink all Firefox profiles
-mklink(){
-  mapfile -t a < <(cygpath -w "${@: -2}")
-  cmd /c mklink ${3+/d} "${a[@]}"
-}
-
-for i in "$APPDATA"/*/*/Profiles/*; do
-  cd "$i"
-  mklink "user.js" "/c/home/GitHub/dotfiles/firefox/user.js"
-  mklink -d "chrome" "/c/home/GitHub/dotfiles/firefox/chrome"
-done
