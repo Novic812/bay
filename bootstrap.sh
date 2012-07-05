38a7c94d7a53ec5f96277a8aad51a80fbd1ab647
Steven Penny <svnpenn@gmail.com>
2012-07-04 21:20:37 -0500

    M  .functions M  bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-# This must not be a function. If you set it as a function and copied it to
-# HOME, then make changes to the function in the repo, any further call would be
-# calling the old function in HOME.
-
 # Script need to be sourced
 [ $SHLVL = 2 ] && echo "Usage: . ${0##*/}" && exit
 
-cd ~/dotfiles
-
 # cp prevents programs from writing to the repo
+pushd ~/dotfiles
 git ls-files -ix ".*" | xargs cp -t ~
 # firefox
 cp -r firefox/. "$APPDATA"/*/*/Profiles/*
@@ -20,4 +15,5 @@ cd
 attrib +h
 
 # Apply
+popd
 . ~/.bash_profile
