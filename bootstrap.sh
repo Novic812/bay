2bd70cfeee1263bd9694bd57680e9486d586796e
Steven Penny <svnpenn@gmail.com>
2012-07-24 18:43:02 -0500

    M bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 
 # cp prevents programs from writing to the repo
 cd /opt/dotfiles
-find -name .\* -type f -exec cp {} ~ \;
+find -name '.*' -type f -exec cp {} ~ \;
 find $APPDATA -name chrome -exec cp -r firefox/. {} \;
 find $APPDATA -name Notepad2 -exec cp -r notepad2/. {} \;
 cd -
