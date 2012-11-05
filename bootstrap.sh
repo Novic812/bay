e7f6f517f159402ada99d9140ebf569d98f7f4b2
Steven Penny <svnpenn@gmail.com>
2012-11-04 20:33:27 -0600

    M .bash_prompt  M .cygwin  M bootstrap.sh ?? .bashrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+. /etc/profile
 
-die(){
-  echo -e "\e[1;31m$1\e[m"
-  exit
-}
-
-[ $SHLVL = 1 ] || die "Usage:  . $0"
 # cp prevents programs from writing to the repo
-cd /opt/dotfiles
+cd ${0%/*}
 cp .* ~
 cp -r bin /usr/local
 cp -r moonch~1 notepad2 $APPDATA 2>/dev/null
-cd -
+echo ! > ~/.bash_history
 
 # git is very picky about the permissions
 cp -r $HOMEDRIVE/dropbox/documents/.ssh ~
 chmod 700 ~/.ssh/id_rsa
-
-# Apply
-. ~/.bash_profile
