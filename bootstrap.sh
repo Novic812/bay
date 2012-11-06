3dc252a910536573383f6a4453051b24b9d2d68a
Steven Penny <svnpenn@gmail.com>
2012-11-05 23:17:45 -0600

    D .bash_history  M bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@
 # cp prevents programs from writing to the repo
 cd ${0%/*}
 cp .* ~
+# do not overwrite existing file
+echo %% >> ~/.bash_history
 cp -r moonch~1 notepad2 $APPDATA 2>/dev/null
 
 # git is very picky about the permissions
