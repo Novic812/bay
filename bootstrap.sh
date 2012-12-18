d37cd34fbe7c52c7dd7746bac44b089e8b427edd
Steven Penny <svnpenn@gmail.com>
2012-12-18 00:28:35 -0600

    M bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 # cp prevents programs from writing to the repo
 read < <(dirname $0)
 cd $REPLY
-ls -A | grep '^\.' | xargs cp -t ~
+compgen -f . | xargs cp -t ~
 
 # do not overwrite existing file
 echo %% >> ~/.bash_history
