73366e0ed91dba516c3de2564ce6de0a91c79856
Steven Penny <svnpenn@gmail.com>
2013-01-28 02:17:46 -0600

    bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,15 @@
 . /etc/profile
 cd -
 
-# cp prevents programs from writing to the repo
+# cp prevents programs from writing to the repo. dirname is needed if you call
+# the script without leading path elements.
 read < <(dirname $0)
 cd $REPLY
 compgen -f . | xargs cp -t ~
 
 # do not overwrite existing file
 echo %% >> ~/.bash_history
-cp -r moonch~1 notepad2 $APPDATA 2>/dev/null
+cp -r 'moonchild productions' notepad2 $APPDATA 2>/dev/null
 
 # git is very picky about the permissions
 cp -r $HOMEDRIVE/dropbox/documents/.ssh ~
