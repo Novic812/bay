fe28a4d52d8f6d8e56e4f0bcda29ac69b3cdf30b
Steven Penny <svnpenn@gmail.com>
2013-02-04 23:17:17 -0600

    M "moonchild productions/pale moon/profiles/default/chrome/usercontent.css"  M "moonchild productions/pale moon/profiles/default/user.js"

--- moonchild productions/pale moon/profiles/default/user.js
+++ moonchild productions/pale moon/profiles/default/user.js
@@ -1,4 +1,7 @@
 /*
-http://techmynd.com/firefox-freezes-for-a-moment-after-every-10-sec-fix
+http://ajagwe.com/firefox-and-ram-aghhhhh
 */
-user_pref("browser.sessionstore.interval", 600000);
+user_pref("browser.cache.disk.capacity", 10000);
+user_pref("browser.cache.memory.capacity", 10000);
+user_pref("browser.sessionhistory.max_entries", 5);
+user_pref("browser.sessionhistory.max_total_viewers", 5);
