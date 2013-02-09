9a2d3c6845e52087a65d80358592441b7e7bd51e
Steven Penny <svnpenn@gmail.com>
2013-02-09 11:08:42 -0600

    M .exports  M .functions  M .path  D "moonchild productions/README"  D "moonchild productions/pale moon/profiles.ini"  D "moonchild productions/pale moon/profiles/default/chrome/stackexchange.xml"  D "moonchild productions/pale moon/profiles/default/chrome/usercontent.css"  D "moonchild productions/pale moon/profiles/default/user.js" ?? mozilla/

--- moonchild productions/pale moon/profiles/default/user.js
+++
@@ -1,7 +0,0 @@
-/*
-http://ajagwe.com/firefox-and-ram-aghhhhh
-*/
-user_pref("browser.cache.disk.capacity", 10000);
-user_pref("browser.cache.memory.capacity", 10000);
-user_pref("browser.sessionhistory.max_entries", 5);
-user_pref("browser.sessionhistory.max_total_viewers", 5);
