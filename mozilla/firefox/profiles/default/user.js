139c6012ce266fae1e6b147487b9896136e75d65
Steven Penny <svnpenn@gmail.com>
2013-02-09 11:09:26 -0600

    D mozilla/firefox/profiles/default/user.js

--- mozilla/firefox/profiles/default/user.js
+++
@@ -1,7 +0,0 @@
-/*
-http://ajagwe.com/firefox-and-ram-aghhhhh
-*/
-user_pref("browser.cache.disk.capacity", 1000);
-user_pref("browser.cache.memory.capacity", 1000);
-user_pref("browser.sessionhistory.max_entries", 2);
-user_pref("browser.sessionhistory.max_total_viewers", 2);
