5978699fcd54ceb7c25d86af89755202cb27231e
Steven Penny <svnpenn@gmail.com>
2013-02-15 10:10:58 -0600

    reset

--- mozilla/reset.sh
+++ mozilla/reset.sh
@@ -1,20 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 # reset Firefox
 
-warn ()
-{
-  printf '\e[36m%s\e[m\n' "$*"
-}
-
-upath ()
-{
-  cd ${!1}
-  read $1 < <(pwd)
-  cd ~-
-}
-
-upath APPDATA
-set $APPDATA mozilla/firefox
-rm -r $1/$2
-cp -r /opt/dotfiles/mozilla $1
-cd $1/$2
+cd $APPDATA
+set mozilla/firefox
+rm -r $1
+cp -r /opt/dotfiles/mozilla .
+cd $1
