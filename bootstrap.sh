8a53c092d2aeddd7e438e28181be49da097a836a
Steven Penny <svnpenn@gmail.com>
2013-02-15 10:13:57 -0600

    M bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-upath ()
-{
-  cd ${!1}
-  read $1 < <(pwd)
-  cd ~-
-}
-
-. /etc/profile
-cd ~-
+# assume PATH is set and HOME exists
 
 # cp prevents programs from writing to the repo. dirname is needed if you call
 # the script without leading path elements.
@@ -18,10 +9,12 @@ compgen -f . | xargs cp -t ~
 
 # do not overwrite existing file
 echo ,, >> ~/.bash_history
-upath APPDATA
-cp -r mozilla notepad2 $APPDATA
+cd $APPDATA
+cd ~-
+cp -r mozilla notepad2 $OLDPWD
 
 # git is very picky about the permissions
-upath HOMEDRIVE
-cp -r $HOMEDRIVE/dropbox/documents/.ssh ~
+cd $HOMEDRIVE
+cd ~-
+cp -r $OLDPWD/dropbox/documents/.ssh ~
 chmod 700 ~/.ssh/id_rsa
