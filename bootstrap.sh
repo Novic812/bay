0c273693a8ced93938cd3462cfa5afd49401475e
Steven Penny <svnpenn@gmail.com>
2013-02-16 01:31:11 -0600

    M bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# assume PATH is set and HOME exists
 
-# cp prevents programs from writing to the repo. dirname is needed if you call
-# the script without leading path elements.
+# assume PATH is set and HOME exists. cp prevents programs from writing to the
+# repo. dirname is needed if you call the script without leading path elements.
 read q < <(dirname $0)
 cd $q
-compgen -f . | xargs cp -t ~
+find -maxdepth 1 -name '.*' -type f -exec cp -t ~ {} +
+cd $APPDATA
+cd -
+find -maxdepth 1 ! -name '.*' -type d -exec cp -rt $OLDPWD {} +
 
 # do not overwrite existing file
 echo ,, >> ~/.bash_history
-cd $APPDATA
-cd ~-
-cp -r mozilla notepad2 $OLDPWD
 
 # git is very picky about the permissions
 cd $HOMEDRIVE
-cd ~-
+cd -
 cp -r $OLDPWD/dropbox/documents/.ssh ~
 chmod 700 ~/.ssh/id_rsa
