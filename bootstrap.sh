b4e01b7a9eb143a7b005018f5a9e8dbbf5bc2636
Steven Penny <svnpenn@gmail.com>
2013-06-17 03:50:22 -0500

    M .cygwin  M .setup  M bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,24 @@
-# disable /etc/profile
-cd /etc
-[ -a profile ] && mv profile profile-disable
+set -e
+mount -c /
+mount -f $PUBLIC/home /home
+mount -f $PUBLIC/opt /opt
+mount -f $PUBLIC/mingw32 /mingw32
+mount -f $PUBLIC/usr/local /usr/local
+mount -m > /etc/fstab
 
-# cp prevents programs from writing to the repo. dirname is needed if you call
-# the script without leading path elements.
-cd $(dirname $0)
+# home
+cd /opt/dotfiles
 find -maxdepth 1 -name '.*' -type f -exec cp -t ~ {} +
+echo init >> ~/.bash_history
+cd $HOMEDRIVE/dropbox/private
+cp -r .ssh bin ~
+chmod 700 ~/.ssh/id_rsa
+
+# appdata
 cd $APPDATA
 cd -
 find -maxdepth 1 ! -name '.*' -type d -exec cp -rt $OLDPWD {} +
 
-# do not overwrite existing file
-echo ,, >> ~/.bash_history
-
-# private data
-cd $HOMEDRIVE/dropbox/private
-cp -r .ssh bin ~
-chmod 700 ~/.ssh/id_rsa
+# etc
+cd /etc
+[ -a profile ] && mv profile profile-disable
