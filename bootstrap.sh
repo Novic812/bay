1c15fd5dadd38b98408ce78651df0bb1b0b38616
Steven Penny <svnpenn@gmail.com>
2013-07-01 03:56:41 -0500

    M bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,6 @@
 mount -c /
-mount -f $HOMEDRIVE/cygwin~/googlecl /googlecl
 mount -f $HOMEDRIVE/cygwin~/home /home
-mount -f $HOMEDRIVE/cygwin~/mingw32 /mingw32
 mount -f $HOMEDRIVE/cygwin~/opt /opt
-mount -f $HOMEDRIVE/cygwin~/php /php
 mount -f $HOMEDRIVE/cygwin~/usr/local /usr/local
 mount -m > /etc/fstab
 
@@ -17,8 +14,7 @@ cp -r $OLDPWD/*/ .
 # private
 cd $HOMEDRIVE/dropbox/private
 cp -r .ssh ~
-chmod 700 ~/.ssh/id_rsa
 
 # etc
 cd /etc
-[ -a profile ] && mv profile profile-disable
+[ -a profile ] && mv profile ~/.profile
