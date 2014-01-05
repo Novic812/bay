4311b536093b67be38d8b96b2cd3dc4f791fdc9b
Steven Penny <svnpenn@gmail.com>
2014-01-05 10:54:31 -0600

    fix #4

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,16 @@
 # in case of emergency
-echo 'PATH=/bin:$PATH PS1="\e];\a\n\e[33m\w\n\e[m# "' >/etc/profile
+echo 'PATH=/bin:/usr/local/bin:$PATH PS1="\e];\a\n\w\n# "' >/etc/profile
 
-# mount /home
+# mount
 mount -f $HOMEDRIVE/cygwin~/home /home
-mkdir -p ~
-
-# install dotfiles
-find -type f -name '.*' -exec cp -t ~ {} +
-echo cd >> ~/.bash_history
-export CYGWIN=nodosfilewarning
-cp -r */ "$APPDATA"
-
-# mount rest
-mount -c /
 mount -f $HOMEDRIVE/cygwin~/srv /srv
 mount -f $HOMEDRIVE/cygwin~/usr/local /usr/local
 mount -m > /etc/fstab
+echo - / cygdrive acl >> /etc/fstab
+
+# install dotfiles
+mkdir -p ~
+echo cd >> ~/.bash_history
+export CYGWIN=nodosfilewarning
+find -maxdepth 1 -type f   -name '.*' -exec cp    -t ~          {} +
+find -maxdepth 1 -type d ! -name '.*' -exec cp -r -t "$APPDATA" {} +
