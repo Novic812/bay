caccb3079945b2eb5eeb70f4cee843f050a0982a
Steven Penny <svnpenn@gmail.com>
2014-02-24 01:55:40 -0600

    -f -va ('"{0}" %1' -f "$env:programfiles/notepad2/notepad2")

--- windows.ps1
+++ windows.ps1
@@ -11,19 +11,19 @@ $keys = 'batfile', 'cmdfile', 'cssfile', 'firefoxhtml', 'inifile', 'jsfile',
 foreach ($key in $keys) {
   sp hklm:software/classes/$key/shell '(default)' edit
   ni hklm:software/classes/$key/shell/edit/command `
-    -f -t expandstring -va '"%programfiles%/notepad2/notepad2" %1'
+    -f -va ('"{0}" %1' -f "$env:programfiles/notepad2/notepad2")
 }
 sp hkcu:software/microsoft/office/14.0/common/internet `
   DoNotCheckIfOfficeIsHTMLEditor 1 -t dword
 
 # smplayer option for folders
 ni hklm:software/classes/directory/shell/smplayer/command `
-  -f -t expandstring -va '"%programfiles(x86)%/smplayer/smplayer" "%1"'
+  -f -va ('"{0}" "%1"' -f "${env:programfiles(x86)}/smplayer/smplayer")
 
 # bash option
 foreach ($key in '', 'background/') {
-  ni hklm:software/classes/directory/${key}shell/bash/command `
-    -f -t expandstring -va 'cmd /c cd %v & start %homedrive%/cygwin64/bin/bash'
+  ni hklm:software/classes/directory/${key}shell/bash/command -f `
+    -va ('{0} saps /cygwin64/bin/bash -wo %v' -f (gcm powershell).definition)
 }
 
 # QuickEdit
@@ -47,12 +47,12 @@ sp 'hkcu:software/microsoft/internet explorer/main/windowssearch' `
   EnabledScopes 0
 rundll32 inetcpl.cpl ClearMyTracksByProcess 1
 
-# the non "system" variables are brittle
+# path
 $0 =
-  '%systemdrive%/program files/7-zip',
-  '%systemdrive%/program files/imagemagick',
-  '%systemroot%/system32',
-  '%systemroot%/system32/windowspowershell/v1.0'
+  "$env:programfiles/7-zip",
+  "$env:programfiles/imagemagick",
+  "$env:windir/system32",
+  "$pshome"
 sp 'hklm:system/currentcontrolset/control/session manager/environment' `
   path ($0 -join ';')
 kill -n explorer
