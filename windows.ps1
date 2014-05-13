4cda03470b79fb65e61c0800a68374630868254a
Steven Penny <svnpenn@gmail.com>
2014-05-12 23:41:19 -0500

    "${env:programfiles}/${env:username}/bin", # ffmpeg

--- windows.ps1
+++ windows.ps1
@@ -50,8 +50,9 @@ rundll32 inetcpl.cpl ClearMyTracksByProcess 1
 
 # path
 $0 =
+  "${env:programfiles}/${env:username}/bin", # ffmpeg
+  "${env:programfiles}/imagemagick",         # ffmpeg
   "${env:programfiles}/7-zip",
-  "${env:programfiles}/imagemagick",
   "${env:programfiles(x86)}/winamp",
   "${env:windir}/system32",
   "${pshome}"
