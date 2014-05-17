64ea1da62150ece72dd52ef5bcefe2e033ec661b
Steven Penny <svnpenn@gmail.com>
2014-05-17 18:57:53 -0500

    sp hklm:/software/classes/$key/shell '(default)' edit

--- windows.ps1
+++ windows.ps1
@@ -10,46 +10,54 @@ $keys = 'batfile', 'cmdfile', 'cssfile', 'firefoxhtml', 'inifile', 'jsfile',
         'microsoft.powershellscript.1', 'msinfofile', 'regfile', 'txtfile',
         'unknown', 'xmlfile'
 foreach ($key in $keys) {
-  sp hklm:software/classes/$key/shell '(default)' edit
-  ni hklm:software/classes/$key/shell/edit/command `
-    -f -va ('"{0}" %1' -f "$env:programfiles/notepad2/notepad2")
+  sp hklm:/software/classes/$key/shell '(default)' edit
+  cd hklm:/software/classes/$key/shell/edit/command
+  ni . -f -va ('"{0}" %1' -f "$env:programfiles/notepad2/notepad2")
 }
-sp hkcu:software/microsoft/office/14.0/common/internet `
-  DoNotCheckIfOfficeIsHTMLEditor 1 -t dword
+cd hkcu:/software/microsoft/office/14.0/common/internet
+sp . DoNotCheckIfOfficeIsHTMLEditor 1 -t dword
 
 # smplayer option for folders
-ni hklm:software/classes/directory/shell/smplayer/command `
-  -f -va ('"{0}" "%1"' -f "$env:programfiles/smplayer/smplayer")
+cd hklm:/software/classes/directory/shell/smplayer/command
+ni . -f -va ('"{0}" "%1"' -f "$env:programfiles/smplayer/smplayer")
 
-# bash option
-foreach ($key in 'shell', 'background/shell') {
+# shell options
+foreach ($key in 'directory', 'directory/background', 'drive') {
+  $0 = (gi $pshome/powershell.exe).fullname
+  cd hklm:/software/classes/$key/shell/powershell/command
+  ni . -f -va "$0 -noe cd '%v'"
+
   $0 = (gi $env:homedrive/cygwin64/bin/cygstart.exe).fullname
-  ni hklm:software/classes/directory/$key/bash/command `
-    -f -va "$0 -d '%v' /bin/bash"
+  cd hklm:/software/classes/$key/shell/bash/command
+  ni . -f -va "$0 -d '%v' /bin/bash"
 }
 
 # QuickEdit
 # - mouse scroll
 # - right click paste
 # - select text with mouse
-sp hkcu:console quickedit 1
-sp hkcu:console WindowPosition 0x01900384
+cd hkcu:/console
+$0 = '%systemroot%_system32_windowspowershell_v1.0_powershell.exe'
+ni $0 -f
+sp $0 ColorTable00   0x00562401
+sp $0 ColorTable07   0x00f0edee
+sp  . QuickEdit      0x00000001
+sp  . WindowPosition 0x01900384
 
 # shortcut extension remove
-sp hkcu:software/microsoft/windows/currentversion/explorer `
-  link ([byte[]](0,0,0,0))
+cd hkcu:/software/microsoft/windows/currentversion/explorer
+sp . link ([byte[]](0,0,0,0))
 
 # desktop background remove picture location history
-$0 = 'hkcu:software/microsoft/windows/currentversion/explorer/wallpapers/images'
-ni $0 -f
-rd $0
+cd hkcu:/software/microsoft/windows/currentversion/explorer/wallpapers
+if (test-path images) {rd images}
 
 # clear internet explorer browsing history
-sp 'hkcu:software/microsoft/internet explorer/main/windowssearch' `
-  EnabledScopes 0
+cd 'hkcu:/software/microsoft/internet explorer/main/windowssearch'
+sp . EnabledScopes 0
 rundll32 inetcpl.cpl ClearMyTracksByProcess 1
 
-# path
+# environment
 $0 =
   "${env:homedrive}/shell/bin",      # ffmpeg
   "${env:programfiles}/imagemagick", # ffmpeg
@@ -57,6 +65,7 @@ $0 =
   "${env:programfiles(x86)}/winamp",
   "${env:windir}/system32",
   "${pshome}"
-sp 'hklm:system/currentcontrolset/control/session manager/environment' `
-  path ($0 -join ';')
+cd 'hklm:/system/currentcontrolset/control/session manager/environment'
+sp . PATH   ($0 -join ';')
+sp . CYGWIN nodosfilewarning
 kill -n explorer
