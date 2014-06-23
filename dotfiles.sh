6bf1aa64f7d3db5e83c72b6ba269e7647ff1b126
Steven Penny <svnpenn@gmail.com>
2014-06-22 19:56:51 -0500

    sed '

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,22 +1,20 @@
 # PATH is defined in ~/.bashrc
 # we must assume this has not happened yet
 PATH=/bin:$PATH
-sw=(
-  "$HOMEDRIVE/Program Files"       "/Program Files"
-  "$HOMEDRIVE/Program Files (x86)" "/Program Files (x86)"
-  "$HOMEDRIVE/Repos"               "/Repos"
-  "$HOMEDRIVE/Shell/bin"           "/Shell/bin"
-  "$HOMEDRIVE/Shell/home"          "/home"
-  "$HOMEDRIVE/Windows"             "/Windows"
-  "$HOMEDRIVE/$USERNAME"           "/$USERNAME"
-)
-set "${sw[@]}"
-while (( $# ))
-do
-  mount -f "$1" "$2"
-  shift 2
-done
-mount -m >/etc/fstab
+sed '
+s,   *,\t,
+s, ,\\040,g
+s,$, . acl,
+' >/etc/fstab <<+
+$HOMEDRIVE/Program Files        /Program Files
+$HOMEDRIVE/Program Files (x86)  /Program Files (x86)
+$HOMEDRIVE/Repos                /Repos
+$HOMEDRIVE/Shell/bin            /Shell/bin
+$HOMEDRIVE/Shell/home           /home
+$HOMEDRIVE/Windows              /Windows
+$HOMEDRIVE/$USERNAME            /$USERNAME
++
+mount -a
 
 # PS1 must be exported before you can use ~
 # we must assume this has not happened yet
