943244dd018ab033f8d3ffa87596aad7c6d45cd3
Steven Penny <svnpenn@gmail.com>
2014-07-23 02:58:52 -0500

    pushd

--- windows.ps1
+++ windows.ps1
@@ -1,11 +1,12 @@
+pushd
 # need if you call from start menu
 cp $PSScriptRoot/profile.ps1 $pshome
 
 # app paths
-cd 'hklm:software/microsoft/windows/currentversion/app paths'
-ni -f -va (join-path $env:homedrive cygwin64/bin/cygstart) cygstart.exe
-ni -f -va (join-path $env:programfiles notepad2/notepad2) notepad2.exe
-ni -f -va (join-path $pshome powershell) powershell.exe
+cd 'hklm:/software/microsoft/windows/currentversion/app paths'
+ni -f -va (cvpa $pshome/powershell.exe) powershell.exe
+ni -f -va (cvpa $env:windir/system32/wbem/wmic.exe) wmic.exe
+ni -f -va (cvpa $env:programfiles/notepad2/notepad2.exe) notepad2.exe
 
 # Notepad2
 $run = @{
@@ -27,10 +28,10 @@ $run.GetEnumerator() | % {
   $k2 = $_.value[0]
   $pm = $_.value[1]
   cd hklm:/software/classes
-  sp $k1 'perceivedtype' $null
-  sp $k1 '(default)'     $k2
-  ni -f "$k2/shell"
-  cd "$k2/shell"
+  si $k1 $k2
+  sp $k1 perceivedtype $null
+  ni -f $k2/shell
+  cd $k2/shell
   if ($pm) {
     ni -va Run 0
     ni -va $pm 0/command
@@ -44,11 +45,12 @@ cd hkcu:/software/microsoft/office/14.0/common
 sp internet DoNotCheckIfOfficeIsHTMLEditor 1 -t d
 
 # shell options
-cd hklm:/software/classes
 'directory', 'directory/background', 'drive' | % {
-  # must be single quote
-  ni $_/shell/bash/command -f -va "cygstart -d '%v' /bin/bash"
-  ni $_/shell/powershell/command -f -va "powershell -noe cd '%v'"
+  cd hklm:/software/classes/$_/shell
+  cvpa "$pshome/powershell.exe", "$env:homedrive/cygwin64/bin/bash.exe" | % {
+    (get-item $_).basename |
+      & {ni -f -va "wmic process call create $_,'%v'" "$input/command"}
+  }
 }
 
 # Console
@@ -70,11 +72,12 @@ cd hkcu:/software/microsoft/windows/currentversion/explorer/wallpapers
 if (test-path images) {rd images}
 
 # need homedrive for man
-$ph =
-  "$env:homedrive/repos/a/misc",
-  "$env:homedrive/shell/bin",
-  "$env:homedrive/windows/system32",
-  "$env:homedrive/windows/system32/windowspowershell/v1.0"
+$ph = & {
+  "$env:homedrive/repos/a/misc"
+  "$env:homedrive/shell/bin"
+  "$env:windir/system32"
+  "$pshome"
+}
 [environment]::setenvironmentvariable('PATH', $ph -join ';', 'm')
 [environment]::setenvironmentvariable('CYGWIN', 'nodosfilewarning', 'm')
 
@@ -82,3 +85,4 @@ $ph =
 cd 'hkcu:/software/microsoft/internet explorer/main'
 sp windowssearch EnabledScopes 0
 rundll32 inetcpl.cpl ClearMyTracksByProcess 1
+popd
