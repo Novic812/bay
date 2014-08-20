7818076cd0ccb7b0fdeee282e84960a095561218
Steven Penny <svnpenn@gmail.com>
2014-08-20 01:38:59 -0500

    # Load Hive

--- hku.ps1
+++ hku.ps1
@@ -1,19 +1,21 @@
+# Load Hive
 $usr = read-host 'Enter user'
-$sid =
-ls 'hklm:/software/microsoft/windows nt/currentversion/profilelist' | ? {
-  $_.getvalue('profileimagepath') -match "$usr$"
-} | select -exp pschildname
 cd registry::hku
 
-# stackoverflow.com/q/4491999
-"$sid/software/microsoft/windows/currentversion/explorer/advanced" | % {
-  sp -vb $_ HideFileExt 0
-}
+join-path hku $usr | % {
+  reg load $_ /users/$usr/ntuser.dat
 
-# superuser.com/q/547832
-"$sid/software/microsoft/office/15.0/common/graphics" | % {
-  ni -f $_
-  sp -vb $_ DisableAnimations 1
+  # stackoverflow.com/q/4491999
+  "$usr/software/microsoft/windows/currentversion/explorer/advanced" | % {
+    sp -vb $_ HideFileExt 0
+  }
+
+  # superuser.com/q/547832
+  "$usr/software/microsoft/office/15.0/common/graphics" | % {
+    ni -f $_
+    sp -vb $_ DisableAnimations 1
+  }
+  reg unload $_
 }
 
 'Press Enter to continue, then log off...'
