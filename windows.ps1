06ce2e588d07b0511a5f8d08c678fe95e6a8dba5
Steven Penny <svnpenn@gmail.com>
2014-11-23 00:38:53 -0600

    Search your feelings, you *know* it to be true!

--- windows.ps1
+++ windows.ps1
@@ -1,12 +1,12 @@
 pushd
 # need if you call from start menu
-cp $PSScriptRoot/profile.ps1 $pshome
+cp $PSScriptRoot\profile.ps1 $pshome
 
 # app paths
-cd 'hklm:/software/microsoft/windows/currentversion/app paths'
+cd 'hklm:\software\microsoft\windows\currentversion\app paths'
 @{
-  "$env:homedrive/cygwin64/bin" = 'bash.exe'
-  "$env:programfiles/notepad2"  = 'notepad2.exe'
+  "$env:homedrive\cygwin64\bin" = 'bash.exe'
+  "$env:programfiles\notepad2"  = 'notepad2.exe'
   "$pshome"                     = 'powershell.exe'
 } | % getEnumerator | % {
   join-path $_.key $_.value | ni -f $_.value
@@ -30,31 +30,31 @@ cd 'hklm:/software/microsoft/windows/currentversion/app paths'
   $k1 = $_.key
   $k2 = $_.value[0]
   $pm = $_.value[1]
-  cd hklm:/software/classes
+  cd hklm:\software\classes
   si $k1 $k2
   sp $k1 perceivedtype $null
-  ni -f $k2/shell
-  cd $k2/shell
+  ni -f $k2\shell
+  cd $k2\shell
   if ($pm) {
     ni -va Run 0
-    ni -va $pm 0/command
+    ni -va $pm 0\command
   }
   ni -va Edit 1
-  ni -va 'notepad2 "%1"' 1/command
+  ni -va 'notepad2 "%1"' 1\command
   ni -va Open 2
-  ni -va 'notepad "%1"' 2/command
+  ni -va 'notepad "%1"' 2\command
 }
-cd hkcu:/software/microsoft/office/14.0/common
+cd hkcu:\software\microsoft\office\14.0\common
 sp internet DoNotCheckIfOfficeIsHTMLEditor 1 -t d
 
 # shell options
-cd hklm:/software/classes
-% -pv 1 {'directory', 'directory/background', 'drive'} |
+cd hklm:\software\classes
+% -pv 1 {'directory', 'directory\background', 'drive'} |
 % -pv 2 {'powershell', 'bash'} |
-% {'cmd /c start /d "%v" {0}' -f $2 | ni -f $1/shell/$2/command}
+% {'cmd /c start /d "%v" {0}' -f $2 | ni -f $1\shell\$2\command}
 
 # Console
-cd hkcu:/
+cd hkcu:\
 sp console QuickEdit 1
 '0x{0:x4}{1:x4}' | % {
   sp console WindowPosition ($_ -f 400,900)
@@ -69,29 +69,29 @@ cd console
 }
 
 # shortcut extension remove
-cd hkcu:/software/microsoft/windows/currentversion
+cd hkcu:\software\microsoft\windows\currentversion
 sp explorer link ([byte[]](0,0,0,0))
 
 # need homedrive for man
 @{
   CYGWIN = 'noDosFileWarning'
   PATH = @(
-    "$env:chocolateyinstall/bin"
-    "$env:homedrive/home/bin"
-    "$env:homedrive/home/git/a/misc"
-    "$env:windir/system32"
+    "$env:chocolateyinstall\bin"
+    "$env:homedrive\home\bin"
+    "$env:homedrive\home\git\a\misc"
+    "$env:windir\system32"
     "$pshome"
-  ) | cvpa -ea 0 | & {$ofs=';';"$input"}
+  ) -join ';'
 } | % getEnumerator | % {
   [environment]::setEnvironmentVariable($_.key, $_.value, 'm')
 }
 
 # clear explorer and wallpaper history
-cd hkcu:/software/microsoft/windows/currentversion/explorer
-rd -ea 0 typedPaths, wallpapers/images
+cd hkcu:\software\microsoft\windows\currentversion\explorer
+rd -ea 0 typedPaths, wallpapers\images
 
 # clear run history
-cd 'hkcu:/software/microsoft/internet explorer/main'
+cd 'hkcu:\software\microsoft\internet explorer\main'
 sp windowssearch EnabledScopes 0
 rundll32 inetcpl.cpl ClearMyTracksByProcess 1
 popd
