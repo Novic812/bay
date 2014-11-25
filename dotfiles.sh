676b76cb5cf991b8e337e34dafe57a1f31196f85
Steven Penny <svnpenn@gmail.com>
2014-11-25 13:35:46 -0600

    while [ -e "$APPDATA"/mozilla/firefox/default/cookies.sqlite-shm ]

--- dotfiles.sh
+++ dotfiles.sh
@@ -13,8 +13,14 @@ mount -a
 mkdir -p "$HOME"
 history -w
 find -maxdepth 1 -type f -name '.*' -exec cp -t "$HOME" {} +
-# FIXME mozilla folder
-cp -r notepad2 "$APPDATA"
+while [ -e "$APPDATA"/mozilla/firefox/default/cookies.sqlite-shm ]
+do
+  (( $# )) || printf 'Please close Firefox'
+  set 0
+  printf .
+  sleep .5
+done
+find -maxdepth 1 -type d ! -name '.*' -exec cp -r -t "$APPDATA" {} +
 
 # restart bash
 pw=$(cygpath -m ~+)
--- mozilla/firefox/default/user.js
+++ mozilla/firefox/default/user.js
@@ -1,84 +1,59 @@
 /*
-newtab.url                        disable new tab page
-places.smartBookmarksVersion      do not generate smart bookmarks
-startup.homepage_override.mstone  disable first run page
-startup.page                      show windows and tabs from last time
-syncPromoViewsLeftMap             disable sync promo
-tabs.drawInTitlebar               title bar
-urlbar.autoFill                   disable URL autocomplete
-urlbar.trimURLs                   do not trim URLs
+newtab.url
+  disable new tab page
+startup.homepage_override.mstone
+  disable first run page
+startup.page
+  show windows and tabs from last time
+tabs.drawInTitlebar
+  title bar
+urlbar.autoFill
+  disable URL autocomplete
+urlbar.trimURLs
+  do not trim URLs
 */
 user_pref('browser.newtab.url', 'about:blank');
-user_pref('browser.places.smartBookmarksVersion', 1);
 user_pref('browser.startup.homepage_override.mstone', 'ignore');
 user_pref('browser.startup.page', 3);
-user_pref('browser.syncPromoViewsLeftMap', '{"addons":0}');
 user_pref('browser.tabs.drawInTitlebar', false);
 user_pref('browser.urlbar.autoFill', false);
 user_pref('browser.urlbar.trimURLs', false);
 
 /*
-dxr.mozilla.org/mozilla-central/source/services/datareporting/policy.jsm
+capability.policy.maonoscript.sites
+  whitelist
+noscript.notify
+  show message about blocked scripts
+noscript.version
+  this is required after install
 */
-user_pref('datareporting.policy.dataSubmissionPolicyResponseTime',
-  '1325397600000');
+user_pref('capability.policy.maonoscript.sites',
+  ' \
+  facebook.com \
+    akamaihd.net \
+  github.com \
+    https://github.com \
+  google.com \
+  gstatic.com \
+  imgur.com \
+    http://imgur.com \
+  jottit.com \
+  reddit.com \
+    redditstatic.com \
+  http://stackoverflow.com \
+    superuser.com \
+    stackexchange.com \
+    sstatic.net \
+    ajax.googleapis.com \
+  youtube.com \
+    ytimg.com \
+  '
+);
+user_pref('noscript.notify', false);
+user_pref('noscript.version', '2.6.9.4');
 
 /*
-currentVersion        this is required after install
-frameobjects          do not show tabs on flash
-hideContributeButton  hide contribute button
-*/
-user_pref('extensions.adblockplus.currentVersion', '2');
-user_pref('extensions.adblockplus.frameobjects', false);
-user_pref('extensions.adblockplus.hideContributeButton', true);
-
-/*
-dimensionlist.outer  current sizes
-*/
-user_pref('extensions.firesizer.dimensionlist.outer', '1050x750');
-
-/*
-fullZoomLevel  default full page zoom level
-textZoomLevel  default text-only zoom level
-hideStatus     current zoom levels in status bar
-prefsVersion   required
-version        thanks for installing nosquint
-rememberSites  forget all per-site settings when firefox closes
-*/
-user_pref('extensions.nosquint.fullZoomLevel', 95);
-user_pref('extensions.nosquint.textZoomLevel', 95);
-user_pref('extensions.nosquint.hideStatus', true);
-user_pref('extensions.nosquint.prefsVersion', '2.0');
-user_pref('extensions.nosquint.version', '2');
-user_pref('extensions.nosquint.rememberSites', false);
-
-/*
-minimum-size.x-western    minimum font size
-name.monospace.x-western  monospace font
-*/
-user_pref('font.minimum-size.x-western', 14);
-user_pref('font.name.monospace.x-western', 'Lucida Console');
-
-/*
-smoothScroll       disable smooth scrolling
-warnOnAboutConfig  about:config warning
-*/
-user_pref('general.smoothScroll', false);
-user_pref('general.warnOnAboutConfig', false);
-
-/*
-css.devPixelsPerPx  display scaling
-*/
-user_pref('layout.css.devPixelsPerPx', '1.19');
-
-/*
-rememberSignons  do not remember passwords
+rememberSignons
+  do not remember passwords
 */
 user_pref('signon.rememberSignons', false);
-
-/*
-status                      do not show status in toolbar
-status.linkOver.delay.show  disable status bar delay
-*/
-user_pref("status4evar.status", 0);
-user_pref('status4evar.status.linkOver.delay.show', 0);
