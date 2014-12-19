8e437571c27495520092a6c40a2865a9d3f299a2
Steven Penny <svnpenn@gmail.com>
2014-12-19 12:17:54 -0600

    vimeo.com http://vimeo.com vimeocdn.com \

--- mozilla/firefox/default/prefs.js
+++ mozilla/firefox/default/prefs.js
@@ -68,6 +68,7 @@ user_pref('capability.policy.maonoscript.sites', ' \
   stackoverflow.com http://stackoverflow.com http://superuser.com \
     http://serverfault.com stackexchange.com http://stackexchange.com \
     http://askubuntu.com sstatic.net mathjax.org \
+  vimeo.com http://vimeo.com vimeocdn.com \
   wikipedia.org wikimedia.org \
   youtube.com ytimg.com allow-any-origin.appspot.com \
 ');
