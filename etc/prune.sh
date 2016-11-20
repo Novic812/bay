6b287a7f28f6809c1a4f367c38b070794b5ff6d1
Steven Penny <svnpenn@gmail.com>
2016-11-20 17:53:47 -0600

    rm -rf /usr/x86_64-w64-mingw32

--- etc/prune.sh
+++ etc/prune.sh
@@ -2,4 +2,4 @@
 j=$(mktemp)
 sage category Base | xargs sage depends | awk '$0=$NF' | sort -u > "$j"
 sage list | grep -vxf "$j" | xargs sage remove
-rm -r /usr/x86_64-w64-mingw32
+rm -rf /usr/x86_64-w64-mingw32
