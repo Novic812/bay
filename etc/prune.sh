208f09acb6cfed845563526c319118712a380f05
Steven Penny <svnpenn@gmail.com>
2016-11-20 18:47:17 -0600

    find /tmp -maxdepth 1 -mindepth 1 -exec rm -r {} +

--- etc/prune.sh
+++ etc/prune.sh
@@ -3,3 +3,4 @@ j=$(mktemp)
 sage category Base | xargs sage depends | awk '$0=$NF' | sort -u > "$j"
 sage list | grep -vxf "$j" | xargs sage remove
 rm -rf /usr/x86_64-w64-mingw32
+find /tmp -maxdepth 1 -mindepth 1 -exec rm -r {} +
