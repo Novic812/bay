938316e1d041eb283392ca72a76f0a044155eb45
Steven Penny <svnpenn@gmail.com>
2016-11-29 20:10:32 -0600

    find /usr/local/bin -type l -! -exec test -e {} ';' -print

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/dash
 find "$PWD" -mindepth 2 -maxdepth 2 '(' -name '*.sh' -o -name '*.awk' ')' \
 -exec ln -sft /usr/local/bin {} +
-find /usr/local/bin -type l -! -exec test -e {} ';' -print
