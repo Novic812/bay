7abde165b51c2b47468dcf2450265b9187e1ba55
Steven Penny <svnpenn@gmail.com>
2016-12-07 20:45:30 -0600

    while read pa

--- music/music-search.sh
+++ music/music-search.sh
@@ -10,14 +10,14 @@ then
   exit
 fi
 
-while read j
+while read pa
 do
-  "$BROWSER" "$j"
-done <<+
+  "$BROWSER" "$pa"
+done <<qu
 youtube.com/results?q="$1 - Topic" intitle:"$2"
 youtube.com/results?q="Clapham Junction" intitle:"$1 $2"
 youtube.com/results?q=-intitle:"$1" "$1" intitle:"$2", hd
 youtube.com/results?q=allintitle:$1 $2, hd
 youtube.com/results?q=allintitle:$1 $2
 google.com/search?q=allintitle:$1 $2
-+
+qu
