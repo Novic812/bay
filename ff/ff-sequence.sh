0322971a97326ea253f0eab45b00378eb1a2fced
Steven Penny <svnpenn@gmail.com>
2016-12-07 20:19:19 -0600

    awk 'BEGIN {

--- ff/ff-sequence.sh
+++ ff/ff-sequence.sh
@@ -1,31 +1,25 @@
 #!/bin/dash -e
 xc() {
-  awk '
-  BEGIN {
-    x = "\47"
-    printf "\33[36m"
-    while (++i < ARGC) {
-      y = split(ARGV[i], z, x)
-      for (j in z) {
-        printf "%s", z[j] ~ /[^[:alnum:]%+,./:=@_-]/ ? x z[j] x : z[j]
+  awk 'BEGIN {
+    x = "\47"; printf "\33[36m"; while (++i < ARGC) {
+      y = split(ARGV[i], z, x); for (j in z) {
+        printf z[j] ~ /[^[:alnum:]%+,./:=@_-]/ ? x z[j] x : z[j]
         if (j < y) printf "\\" x
-      }
-      printf i == ARGC - 1 ? "\33[m\n" : FS
+      } printf i == ARGC - 1 ? "\33[m\n" : FS
     }
-  }
-  ' "$@" | fmt -80
+  }' "$@" | fmt -80
   "$@"
 }
 
 if [ "$#" != 3 ]
 then
-  cat <<+
+  cat <<'br'
 SYNOPSIS
   ff-sequence.sh [start] [duration] [file]
 
 DESCRIPTION
   Make an image sequence from a video
-+
+br
   exit
 fi
 
