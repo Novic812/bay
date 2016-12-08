7c69ba4df60e0563fbb296cff9793fcccf00562a
Steven Penny <svnpenn@gmail.com>
2016-12-07 20:43:49 -0600

    modified:   etc/screen-buffer.sh

--- etc/screen-buffer.sh
+++ etc/screen-buffer.sh
@@ -1,25 +1,21 @@
 #!/bin/dash -e
-if [ "$#" != 2 ]
-then
-  ju=$(reg query 'hkcu\console' /v screenBufferSize | awk '$0=$3')
-  cat <<+
-screen-buffer.sh [rows] [columns]
-max rows is 32767
-max columns is 170
-current buffer rows $((ju >> 0x0010))
-current buffer columns $((ju & 0xFFFF))
-+
-  exit
-fi
-ki=$1
-li=$2
 
-mi() {
+wh() {
   printf '0x%04x%04x' "$@"
 }
 
-reg add 'hkcu\console' /f /v screenBufferSize /t reg_dword \
-  /d "$(mi "$ki" "$li")"
-reg add 'hkcu\console' /f /v windowSize /t reg_dword \
-  /d "$(mi 24 "$li")"
+if [ "$#" != 2 ]
+then
+  xr=$(reg query 'hkcu\console' /v screenBufferSize | awk '$0=$3')
+  cat <<ya
+screen-buffer.sh [rows] [columns]
+max rows is 32767
+max columns is 170
+current buffer rows $((xr >> 0x0010))
+current buffer columns $((xr & 0xFFFF))
+ya
+  exit
+fi
+reg add 'hkcu\console' /f /v screenBufferSize /t reg_dword /d "$(wh "$1" "$2")"
+reg add 'hkcu\console' /f /v windowSize /t reg_dword /d "$(wh 24 "$2")"
 echo 'you must close shell to apply changes'
