51d7c0afa4432ae7d7d4294868151623e404e6b6
Steven Penny <svnpenn@gmail.com>
2016-12-14 23:19:44 -0600

    echo 'gmail.sh [from name] [user] [password] [URL]'

--- etc/gmail.sh
+++ etc/gmail.sh
@@ -1,19 +1,45 @@
 #!/bin/dash -e
 # turn on access for less secure apps
 # https://www.google.com/settings/security/lesssecureapps
-if [ "$#" != 7 ]
+if [ "$#" != 4 ]
 then
-  echo 'gmail.sh [from] [user] [password]' \
-  '[to] [subject] [references] [body file]'
+  echo 'gmail.sh [from name] [user] [password] [URL]'
   exit
 fi
-fro=$1 use=$2 pas=$3 to=$4 sub=$5 ref=$6 bod=$7
-q=$(mktemp /tmp/gmail-XXX.txt)
-cat - "$bod" > "$q" <<eof
-from: $fro
-to: $to
-subject: $sub
-references: $ref
-
-eof
-curl --mail-rcpt "$to" --upload-file "$q" smtps://"$use":"$pas"@smtp.gmail.com
+fro=$1 use=$2 pas=$3 url=$4
+uf=$(mktemp /tmp/upload-file-XXX.txt)
+mr=$(mktemp /tmp/mail-rcpt-XXX.txt)
+curl "$url" |
+awk '
+BEGIN {
+  ARGC = 0
+  FS = OFS = ": "
+}
+$1 == "Date" {split($2, wh, / [-+]/)}
+$1 == "From" {
+  split($2, xr, " <")
+  $2 = ARGV[1]
+  print
+}
+$1 == "To" {
+  sub(" at ", "@")
+  sub(" dot ", ".")
+  print
+  print $2 > ARGV[2]
+}
+$1 == "Subject" {
+  $2 = "Re: " $2
+  print
+}
+$1 == "Message-Id" {
+  $1 = "References"
+  print
+}
+!ya && !$0 {ya = NR}
+ya && NR == ya + 1 {printf "On %s, %s wrote:\n", wh[1], xr[1]}
+ya && $0 {$0 = "> " $0}
+ya
+' "$fro" "$mr" > "$uf"
+"$EDITOR" "$uf"
+read mr < "$mr"
+curl --mail-rcpt "$mr" --upload-file "$uf" smtps://"$use":"$pas"@smtp.gmail.com
