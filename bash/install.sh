9770838c8886c6958ff41a2e717d4eb90d503c19
Steven Penny <svnpenn@gmail.com>
2016-12-18 12:42:14 -0600

    ln -sfv "$PWD"/fstab /etc

--- bash/install.sh
+++ bash/install.sh
@@ -1,38 +1,16 @@
 #!/bin/dash -e
-PATH=/bin
-export CYGWIN=winsymlinks:native
 
 # /etc
-ln -sf "$PWD"/fstab /etc
+ln -sfv "$PWD"/fstab /etc
+mount -a
 
 # /home
 mkdir -p ~
-if [ ! -e .bash_history -a -e ~/.bash_history ]
-then
-  mv ~/.bash_history .
-else
-  >> .bash_history
+if [ ! -f .bash_history -a -f ~/.bash_history ]
+then mv ~/.bash_history .
+else >> .bash_history
 fi
-find "$PWD" -type f -name '.*' -exec ln -sft ~ {} +
+ln -sfv "$PWD"/.bash_history "$PWD"/.bashrc "$PWD"/.inputrc ~
 
 # /usr
-xargs ln -fsvt /usr/local/bin <<eof
-"$PROGRAMFILES/gpac/mp4box.exe"
-"$PROGRAMFILES/mozilla firefox/firefox.exe"
-"$PROGRAMFILES/notepad2/notepad2.exe"
-"$SYSTEMROOT/system32/ping.exe"
-"$SYSTEMROOT/system32/reg.exe"
-"$SYSTEMROOT/system32/rundll32.exe"
-"$SYSTEMROOT/system32/schtasks.exe"
-"$SYSTEMROOT/system32/setx.exe"
-eof
-
-for q in /usr/local/bin/*
-do
-  if [ ! -f "$q" ]
-  then rm -v "$q"
-  fi
-done
-
-# close
-echo 'you must close shell to apply changes'
+xargs -d '\n' ln -sfvt /usr/local/bin < symlink.txt
