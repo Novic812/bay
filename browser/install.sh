2194fd531e98cb69cfe391784bf7d944de02b484
Steven Penny <svnpenn@gmail.com>
2016-12-20 20:30:52 -0600

    install -Dvt "$APPDATA"/mozilla/firefox/0 prefs.js

--- browser/install.sh
+++ browser/install.sh
@@ -1,5 +1,3 @@
 #!/bin/dash -e
 install -Dvt "$APPDATA"/mozilla/firefox profiles.ini
-install -Dvt "$APPDATA"/mozilla/firefox/0 prefs.js
-install -Dvt "$APPDATA"/mozilla/firefox/1 prefs.js
 ln -sfv "$PWD"/.browser ~
