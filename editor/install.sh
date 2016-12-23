4b8282518de0b4c12544be1463e7949e18ae9ed6
Steven Penny <svnpenn@gmail.com>
2016-12-23 00:28:45 -0600

    install -Dvt "$APPDATA"/Notepad2 Notepad2.ini

--- editor/install.sh
+++ editor/install.sh
@@ -1,4 +1,4 @@
 #!/bin/dash -e
-install -Dvt "$APPDATA"/notepad2 notepad2.ini
-ln -sfv "$PWD"/notepad2.sh /usr/local/bin
+install -Dvt "$APPDATA"/Notepad2 Notepad2.ini
+ln -sfv "$PWD"/Notepad2.awk /usr/local/bin
 ln -sfv "$PWD"/.editor ~
