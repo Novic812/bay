61d41dd404c09e7115718bed4cd6ceff4b76450c
Steven Penny <svnpenn@gmail.com>
2016-12-27 23:37:12 -0600

    "$z"/Slider/background.png "$z"/Slider/knob.png

--- volume/install.sh
+++ volume/install.sh
@@ -5,6 +5,6 @@ z=$q/'3RVX/Skins/Windows Default'
 mkdir -pv "$z"/OSD "$z"/Slider
 ln -sfv "$PWD"/skin.xml "$z"
 touch "$z"/sound.wav "$z"/OSD/eject.png "$z"/OSD/knob.png "$z"/OSD/mute.png \
-"$z"/Slider/background.png "$z"/Slider/knob.png
+  "$z"/Slider/background.png "$z"/Slider/knob.png
 wget -nc -B https://raw.githubusercontent.com/malensek/3RVX/master/ \
--i volume.txt -P "$z"/OSD
+  -i volume.txt -P "$z"/OSD
