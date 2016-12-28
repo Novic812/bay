112a40fca29d4c4d20e66dd050aa343ad503df52
Steven Penny <svnpenn@gmail.com>
2016-12-28 00:16:39 -0600

    q=$SYSTEMDRIVE/'Program Files (x86)/3RVX/Skins'

--- volume/install.sh
+++ volume/install.sh
@@ -1,10 +1,8 @@
 #!/bin/dash -e
-q=$SYSTEMDRIVE/'Program Files (x86)'
-z=$q/'3RVX/Skins/Windows Default'
+q=$SYSTEMDRIVE/'Program Files (x86)/3RVX/Skins'
+z=$q/'Windows Default'
 [ -d "$q" ]
-mkdir -pv "$z"/OSD "$z"/Slider
-ln -sfv "$PWD"/skin.xml "$z"
-touch "$z"/sound.wav "$z"/OSD/eject.png "$z"/OSD/knob.png "$z"/OSD/mute.png \
-  "$z"/Slider/background.png "$z"/Slider/knob.png
+mkdir -pv "$z"
+cp -rv OSD Slider sound.wav skin.xml "$z"
 wget -nc -B https://raw.githubusercontent.com/malensek/3RVX/master/ \
   -i volume.txt -P "$z"/OSD
