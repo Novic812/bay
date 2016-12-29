f98d558d4af0c7a1e950186c81106d50059c98dd
Steven Penny <svnpenn@gmail.com>
2016-12-28 21:28:29 -0600

    mkdir -pv "$z"/OSD

--- volume/install.sh
+++ volume/install.sh
@@ -2,7 +2,7 @@
 q=$SYSTEMDRIVE/'Program Files (x86)/3RVX/Skins'
 z=$q/'Windows Default'
 [ -d "$q" ]
-mkdir -pv "$z"
-cp -rv OSD Slider sound.wav skin.xml "$z"
+mkdir -pv "$z"/OSD
+ln -sfv "$PWD"/skin.xml "$z"
 wget -nc -B https://raw.githubusercontent.com/malensek/3RVX/master/ \
   -i volume.txt -P "$z"/OSD
