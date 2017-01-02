d99bd1c6ec35310cbce9a5b162b330d61143c754
Steven Penny <svnpenn@gmail.com>
2017-01-01 21:27:33 -0600

    function a(d,   h, k, q, x, z) {

--- music/music-art.sh
+++ music/music-art.sh
@@ -11,10 +11,10 @@ then
 fi
 
 awk '
-function a(d, h) {
-  k = "\47"; q = split(d, x, k)
-  for (z in x) h = h k x[z] k (z < q ? "\\" k : "")
-  return h
+function a(d,   h, k, q, x, z) {
+  h = "\47"; k = split(d, q, h)
+  for (x in q) z = z h q[x] h (x < k ? "\\" h : "")
+  return z
 }
 {
   gsub(" ", "+")
