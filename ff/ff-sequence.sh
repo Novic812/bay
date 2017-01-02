2d8f720372c2c5f53c9fec257ee46e079116dc63
Steven Penny <svnpenn@gmail.com>
2017-01-02 12:35:16 -0600

    awk 'BEGIN {d = "\47"; printf "\33[36m"; while (++q < ARGC)

--- ff/ff-sequence.sh
+++ ff/ff-sequence.sh
@@ -1,9 +1,9 @@
 #!/bin/dash -e
 xc() {
-  awk 'BEGIN {d = "\47"; printf "\33[36m"; while (++j < ARGC) {
-  k = split(ARGV[j], q, d); q[1]; for (x in q) printf "%s%s",
-  q[x] ~ /^[[:alnum:]%+,./:=@_-]+$/ ? q[x] : d q[x] d, x < k ? "\\" d : ""
-  printf j == ARGC - 1 ? "\33[m\n" : FS}}' "$@" | fmt -80
+  awk 'BEGIN {d = "\47"; printf "\33[36m"; while (++q < ARGC) {
+  x = split(ARGV[q], y, d); y[1]; for (z in y) printf "%s%s",
+  !x || y[z] ~ /[^[:alnum:]%+,./:=@_-]/ ? d y[z] d : y[z], z < x ? "\\" d : ""
+  printf q == ARGC - 1 ? "\33[m\n" : FS}}' "$@"
   "$@"
 }
 
