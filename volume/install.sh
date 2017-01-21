20f14326fc340058fe4e428b8280f10643298172
Steven Penny <svnpenn@gmail.com>
2017-01-20 23:46:04 -0600

    q=$SYSTEMDRIVE'/Program Files (x86)/3RVX/Skins'

--- volume/install.sh
+++ volume/install.sh
@@ -1,9 +1,14 @@
 #!/bin/dash -e
-q=$SYSTEMDRIVE/'Program Files (x86)/3RVX/Skins'
-z=$q/'Windows Default'
+q=$SYSTEMDRIVE'/Program Files (x86)/3RVX/Skins'
+z=$q'/Windows Default'
 [ -d "$q" ]
 mkdir -pv "$z"/OSD
 ln -sfv "$PWD"/skin.xml "$z"
-wget -nc -B https://raw.githubusercontent.com/malensek/3RVX/master/ \
-  -i volume.txt -P "$z"/OSD
 ln -sfv "$PWD"/settings.xml "$APPDATA"/3RVX
+cd "$z"/OSD
+set *.png
+[ ! -f "$1" ]
+wget -B https://raw.githubusercontent.com/malensek/3RVX/master/ -i- <<'eof'
+Skins/Windows Default/OSD/background.png
+Skins/Windows Default/OSD/bar.png
+eof
