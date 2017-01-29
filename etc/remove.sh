25bfa9c389b64c458d2c6162ce725c5e17086c09
Steven Penny <svnpenn@gmail.com>
2017-01-29 10:11:29 -0600

    xtrace() {

--- etc/remove.sh
+++ etc/remove.sh
@@ -1,9 +1,9 @@
 #!/bin/dash -e
-xc() {
-  awk 'BEGIN {d = "\47"; printf "\33[36m"; while (++j < ARGC) {
-  k = split(ARGV[j], q, d); q[1]; for (x in q) printf "%s%s",
-  q[x] ~ /^[[:alnum:]%+,./:=@_-]+$/ ? q[x] : d q[x] d, x < k ? "\\" d : ""
-  printf j == ARGC - 1 ? "\33[m\n" : FS}}' "$@"
+xtrace() {
+  awk 'BEGIN {d = "\47"; printf "\33[36m"; while (++q < ARGC) {
+  x = split(ARGV[q], y, d); y[1]; for (z in y) printf "%s%s",
+  !x || y[z] ~ /[^[:alnum:]%+,./:=@_-]/ ? d y[z] d : y[z], z < x ? "\\" d : ""
+  printf q == ARGC - 1 ? "\33[m\n" : FS}}' "$@"
   "$@"
 }
 
@@ -11,7 +11,7 @@ xc() {
 while read br
 do
   if REG QUERY "$br" >/dev/null 2>&1
-  then xc REG DELETE "$br" /f
+  then xtrace REG DELETE "$br" /f
   fi
 done <<'eof'
 HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\TypedPaths
@@ -19,7 +19,7 @@ HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers\Images
 eof
 
 # clear run history
-xc rundll32 inetcpl.cpl ClearMyTracksByProcess 1
+xtrace rundll32 inetcpl.cpl ClearMyTracksByProcess 1
 
 # remove dead symlinks
 for br in /usr/local/bin/*
