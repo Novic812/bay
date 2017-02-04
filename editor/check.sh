416d547b3b7d876491173b52d677dcdb0351226f
Steven Penny <svnpenn@gmail.com>
2017-02-03 22:55:32 -0600

    . stdlib.sh

--- editor/check.sh
+++ editor/check.sh
@@ -1,18 +1,12 @@
 #!/bin/dash -e
-xc() {
-  awk 'BEGIN {d = "\47"; printf "\33[36m"; while (++j < ARGC) {
-  k = split(ARGV[j], q, d); q[1]; for (x in q) printf "%s%s",
-  q[x] ~ /^[[:alnum:]%+,./:=@_-]+$/ ? q[x] : d q[x] d, x < k ? "\\" d : ""
-  printf j == ARGC - 1 ? "\33[m\n" : FS}}' "$@"
-  "$@"
-}
-xc "$EDITOR"
-xc "$EDITOR" ''
-xc "$EDITOR" /etc/wgetrc
-xc "$EDITOR" 'C:\cygwin64\etc\shells'
-xc "$EDITOR" "$(mktemp /tmp/\'XXX.txt)"
-xc git clone --depth 1 git://github.com/svnpenn/a
-xc cd a
+. stdlib.sh
+xtrace "$EDITOR"
+xtrace "$EDITOR" ''
+xtrace "$EDITOR" /etc/wgetrc
+xtrace "$EDITOR" 'C:\cygwin64\etc\shells'
+xtrace "$EDITOR" "$(mktemp /tmp/\'XXX.txt)"
+xtrace git clone --depth 1 git://github.com/svnpenn/a
+xtrace cd a
 echo EDITOR >> readme.md
-xc git commit -a
-xc hub pull-request
+xtrace git commit -a
+xtrace hub pull-request
