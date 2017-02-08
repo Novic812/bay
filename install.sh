a6fe916bd32fbcda325b68eb8605ac8c0c56b71b
Steven Penny <svnpenn@gmail.com>
2017-02-07 20:39:28 -0600

    q=$SYSTEMDRIVE'/Program Files (x86)/3RVX/Skins'

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/dash -e
 # we need to install shell first for /usr/local/bin
-. shell/.profile
-for q in shell browser editor git reg volume
+. bash/.profile
+for q in bash 3rvx firefox git notepad2 reg
 do
   cd "$q"
   ./install.sh
