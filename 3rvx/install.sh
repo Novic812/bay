c8a830f1a7acb4fa20574482d21a65adeaee1c4e
Steven Penny <svnpenn@gmail.com>
2017-02-11 12:07:59 -0600

    if [ ! -f "$1" ]

--- 3rvx/install.sh
+++ 3rvx/install.sh
@@ -7,8 +7,10 @@ ln -sfv "$PWD"/skin.xml "$z"
 ln -sfv "$PWD"/settings.xml "$APPDATA"/3RVX
 cd "$z"/OSD
 set *.png
-[ ! -f "$1" ]
-wget -B https://raw.githubusercontent.com/malensek/3RVX/master/ -i- <<'eof'
+if [ ! -f "$1" ]
+then
+  wget -B https://raw.githubusercontent.com/malensek/3RVX/master/ -i- <<'eof'
 Skins/Windows Default/OSD/background.png
 Skins/Windows Default/OSD/bar.png
 eof
+fi
