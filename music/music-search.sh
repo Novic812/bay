81abad4bb9cff6a057a033c56144afcbb38b8fc2
Steven Penny <svnpenn@gmail.com>
2017-03-11 08:08:17 -0600

    youtube.com/results?q=intext:"$1 - Topic" intitle:"$2"

--- music/music-search.sh
+++ music/music-search.sh
@@ -1,4 +1,5 @@
 #!/bin/dash
+# Firefox exits 1 if already open
 if [ ! "$BROWSER" ]
 then
   echo 'BROWSER not set or not exported'
@@ -13,7 +14,7 @@ fi
 while read z
 do "$BROWSER" "$z"
 done <<eof
-youtube.com/results?q="$1 - Topic" intitle:"$2"
+youtube.com/results?q=intext:"$1 - Topic" intitle:"$2"
 youtube.com/results?q="Clapham Junction" intitle:"$1 $2"
 youtube.com/results?q=-intitle:"$1" "$1" intitle:"$2", hd
 youtube.com/results?q=allintitle:$1 $2, hd
