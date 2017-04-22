2bdb1b444fc1ab2d2b33d2221f36d6e42f6650a6
Steven Penny <svnpenn@gmail.com>
2017-04-22 18:15:18 -0500

    inr=$1

--- img/img-circle.sh
+++ img/img-circle.sh
@@ -5,12 +5,18 @@ then
   exit 1
 fi
 
-qu=$(($1*2))
-ro=$(($5*2))
+inr=$1
+cdx=$2
+cdy=$3
+inf=$4
+otr=$5
+otf=$6
+ind=$((inr * 2))
+otd=$((otr * 2))
 
-convert "$4" -extent ${qu}x${qu}+${2}+${3} \
-  '(' +clone -alpha transparent -draw "circle $1,$1 $1,0" ')' \
-  -compose copyopacity -composite -resize ${ro}x${ro} \
-  -define icon:auto-resize=256,48,16 -compress zip "$6"
+convert "$inf" -extent "$ind"x"$ind"+"$cdx"+"$cdy" \
+'(' +clone +alpha transparent -draw "circle $inr,$inr $inr,0" ')' \
+-compose copyopacity -composite -resize "$otd"x"$otd" \
+-define icon:auto-resize=256,48,16 -compress zip "$otf"
 
 ie4uinit -ClearIconCache
