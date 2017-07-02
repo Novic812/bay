6641bf6881f56d6af67638ef50e2ffb3f0fd24bd
Steven Penny <svnpenn@gmail.com>
2017-07-01 20:12:05 -0500

    // disable delay hiding mute tab

--- firefox/browser-console.js
+++ firefox/browser-console.js
@@ -15,8 +15,8 @@ qu = {
   'browser.startup.homepage_override.mstone': 'ignore',
   // show windows and tabs from last time
   'browser.startup.page': 3,
-  // disable mute tab
-  'browser.tabs.showAudioPlayingIcon': false,
+  // disable delay hiding mute tab
+  'browser.tabs.delayHidingAudioPlayingIconMS': 0,
   // title bar
   'browser.tabs.drawInTitlebar': false,
   // jumplist setting
