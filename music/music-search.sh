d35414bb752492b4a4aee5f271f1abd1b73a75b1
Steven Penny <svnpenn@gmail.com>
2017-09-04 16:55:10 -0500

    google.com/search?tbm=vid&q="$1 - Topic" "$2"

--- music/music-search.sh
+++ music/music-search.sh
@@ -15,9 +15,9 @@ while read z
 do "$BROWSER" "$z"
 done <<eof
 youtube.com/results?q=intext:"$1 - Topic" intitle:"$2"
-youtube.com/results?q="Clapham Junction" intitle:"$1 $2"
 youtube.com/results?q=-intitle:"$1" "$1" intitle:"$2", hd
 youtube.com/results?q=allintitle:$1 $2, hd
 youtube.com/results?q=allintitle:$1 $2
+google.com/search?tbm=vid&q="$1 - Topic" "$2"
 google.com/search?q=allintitle:$1 $2
 eof
