eea3197e14c6f2e6a2544c529d6b27ca3aed7c69
Steven Penny <svnpenn@gmail.com>
2017-10-16 22:22:05 -0500

    xr=$(mktemp XXX.mp4)

--- ff/ff-loop.sh
+++ ff/ff-loop.sh
@@ -4,7 +4,13 @@ then
   echo 'ff-loop.sh <image> <audio>'
   exit 1
 fi
-q=$(mktemp XXX.mp4)
-z=$(ffprobe -v 0 -of csv=p=0 -show_entries format=duration "$2" | tr -d '\r')
-ffmpeg -y -hide_banner -loop 1 -r 1 -i "$1" -i "$2" -t "$z" -c:a copy \
--pix_fmt yuv420p -vf 'scale=-2:min(1080\,ih)' "$q"
+xr=$(mktemp XXX.mp4)
+ya=$(ffprobe -v 0 -of csv=p=0 -show_entries format=duration "$2" | tr -d '\r')
+
+if [ "${2%m4a}" = "$2" ]
+then zu=libfdk_aac
+else zu=copy
+fi
+
+ffmpeg -y -hide_banner -loop 1 -r 1 -i "$1" -i "$2" -t "$ya" -c:a "$zu" \
+-pix_fmt yuv420p -vf 'scale=-2:min(1080\,ih)' "$xr"
