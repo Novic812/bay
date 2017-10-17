9e321b4f17a03e8d93c1abe5372cdc739cace219
Steven Penny <svnpenn@gmail.com>
2017-10-16 21:30:07 -0500

    z=$(ffprobe -v 0 -of csv=p=0 -show_entries format=duration "$2

--- ff/ff-loop.sh
+++ ff/ff-loop.sh
@@ -5,6 +5,6 @@ then
   exit 1
 fi
 q=$(mktemp XXX.mp4)
-z=$(ffprobe -v 0 -of csv=p=0 -show_entries stream=duration "$2" | tr -d '\r')
+z=$(ffprobe -v 0 -of csv=p=0 -show_entries format=duration "$2" | tr -d '\r')
 ffmpeg -y -hide_banner -loop 1 -r 1 -i "$1" -i "$2" -t "$z" -c:a copy \
 -pix_fmt yuv420p -vf 'scale=-2:min(1080\,ih)' "$q"
