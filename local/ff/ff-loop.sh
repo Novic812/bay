3ee4bce2eaab7f1143470058c73f1b5bf7cd2af1
Steven Penny <svnpenn@gmail.com>
2017-11-01 20:55:04 -0500

    *.m4a|*.mp3) zu=copy ;;

--- local/ff/ff-loop.sh
+++ local/ff/ff-loop.sh
@@ -7,10 +7,10 @@ fi
 xr=$(mktemp XXX.mp4)
 ya=$(ffprobe -v 0 -of csv=p=0 -show_entries format=duration "$2" | tr -d '\r')
 
-if [ "${2%m4a}" = "$2" ]
-then zu=libfdk_aac
-else zu=copy
-fi
+case $2 in
+*.m4a|*.mp3) zu=copy ;;
+*) zu=libfdk_aac ;;
+esac
 
 ffmpeg -y -hide_banner -loop 1 -r 1 -i "$1" -i "$2" -t "$ya" -c:a "$zu" \
 -pix_fmt yuv420p -vf 'scale=-2:min(1080\,ih)' "$xr"
