cd36037a516951deb7cafe9e9ae8f98fb62c0933
Steven Penny <svnpenn@gmail.com>
2018-01-03 23:39:01 -0600

    case $1 in

--- config/firefox/firefox.sh
+++ config/firefox/firefox.sh
@@ -1,7 +1,13 @@
 #!/bin/dash -e
-if [ "${1#/}" != "$1" ]
-then k=$(cygpath -w "$1")
-elif [ "$#" != 0 ]
-then k=$1
-fi
+case $1 in
+/*)
+  k=$(cygpath -w "$1")
+;;
+*)
+  if [ "$#" != 0 ]
+  then
+    k=$1
+  fi
+;;
+esac
 exec firefox ${k+"$k"}
