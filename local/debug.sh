88fdfe4e9cee800f3935fcb42b536012ebf7f78d
Steven Penny <svnpenn@gmail.com>
2018-03-07 18:14:53 -0600

    new file:   debug.sh

---
+++ local/debug.sh
@@ -0,0 +1,18 @@
+#!/bin/dash -e
+if [ "$#" != 1 ]
+then
+  echo 'debug.sh <file>'
+  exit 1
+fi
+y=$1
+
+awk -D/dev/stdin -f "$y" <<'eof' |
+dump
+q
+eof
+awk '
+/] / {
+  split($0, z, /] /)
+  print z[2]
+}
+'
