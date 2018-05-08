16c0ef28138c262ead3db54060714bbc59ad8f92
Steven Penny <svnpenn@gmail.com>
2018-05-07 22:30:44 -0500

    new file:   local/multimedia/mm-audio.sh

---
+++ local/multimedia/mm-audio.sh
@@ -0,0 +1,24 @@
+#!/bin/dash -e
+if [ "$#" -lt 3 ]
+then
+  cat <<'eof'
+SYNOPSIS
+  mm-audio.sh <in format> <out format> <in file>...
+
+FORMAT
+  flac
+  m4a
+  mp3
+eof
+  exit 1
+fi
+
+q=$1
+v=$2
+shift 2
+
+for w
+do
+  z=$(basename "$w" "$q")
+  k-trace ffmpeg -i "$w" -b:a 256k "$z$v"
+done
