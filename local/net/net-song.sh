1cbd629127dc772d5d19e6ce01b153255e536593
Steven Penny <svnpenn@gmail.com>
2018-06-09 23:22:18 -0500

    cat <<'eof'

--- local/net/net-song.sh
+++ local/net/net-song.sh
@@ -3,16 +3,30 @@ if [ ! "$BROWSER" ]
 then
   echo 'BROWSER not set or not exported'
   exit 1
-elif [ "$#" != 2 ]
+elif [ "$#" != 3 ]
 then
-  echo 'net-song.sh <artist> <song>'
+  cat <<'eof'
+SYNOPSIS
+  net-song.sh <type> <artist> <song>
+
+TYPE
+  1   initial search
+  2   upgrade search
+eof
   exit 1
 fi
 
+# initial and upgrade
 "$BROWSER" \
--new-tab "youtube.com/results?q=intext:\"$1 - Topic\" intitle:\"$2\"" \
--new-tab "youtube.com/results?q=-intitle:\"$1\" \"$1\" intitle:\"$2\", hd" \
--new-tab "youtube.com/results?q=allintitle:$1 $2, hd" \
--new-tab "youtube.com/results?q=allintitle:$1 $2" \
--new-tab "google.com/search?tbm=vid&q=\"$1 - Topic\" \"$2\"" \
--new-tab "google.com/search?q=allintitle:$1 $2"
+-new-tab "youtube.com/results?q=intext:\"$2 - Topic\" intitle:\"$3\"" \
+-new-tab "youtube.com/results?q=-intitle:\"$2\" \"$2\" intitle:\"$3\", hd" \
+-new-tab "google.com/search?tbm=vid&q=\"$2 - Topic\" \"$3\""
+
+# initial only
+if [ "$1" = 1 ]
+then
+  "$BROWSER" \
+  -new-tab "youtube.com/results?q=allintitle:$2 $3, hd" \
+  -new-tab "youtube.com/results?q=allintitle:$2 $3" \
+  -new-tab "google.com/search?q=allintitle:$2 $3"
+fi
