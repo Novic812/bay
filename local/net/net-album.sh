4610c03c9c96f1022b5a439cf72677518b523461
Steven Penny <svnpenn@gmail.com>
2018-06-10 09:07:05 -0500

    echo 'net-album.sh <artist> <album>'

--- local/net/net-album.sh
+++ local/net/net-album.sh
@@ -3,16 +3,16 @@ if [ ! "$BROWSER" ]
 then
   echo 'BROWSER not set or not exported'
   exit 1
-elif [ "$#" != 1 ]
+elif [ "$#" != 2 ]
 then
-  echo 'net-album.sh <album>'
+  echo 'net-album.sh <artist> <album>'
   exit 1
 fi
 
 "$BROWSER" \
--new-tab google.com/search?q="\"$1\" \"btih\"" \
--new-tab google.com/search?q="\"$1\" \"flac\"" \
--new-tab google.com/search?q="\"$1\" \"m4a\"" \
--new-tab google.com/search?q="\"$1\" \"magnet\"" \
--new-tab google.com/search?q="\"$1\" \"mp3\"" \
--new-tab google.com/search?q="\"$1\" \"torrent\""
+-new-tab google.com/search?q="\"$1\" \"$2\" \"btih\"" \
+-new-tab google.com/search?q="\"$1\" \"$2\" \"flac\"" \
+-new-tab google.com/search?q="\"$1\" \"$2\" \"m4a\"" \
+-new-tab google.com/search?q="\"$1\" \"$2\" \"magnet\"" \
+-new-tab google.com/search?q="\"$1\" \"$2\" \"mp3\"" \
+-new-tab google.com/search?q="\"$1\" \"$2\" \"torrent\""
