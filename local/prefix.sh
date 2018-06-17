b0e48f2095165101efb024086c60977b29c5f6b8
Steven Penny <svnpenn@gmail.com>
2018-06-16 22:31:28 -0500

    q=$1

--- local/prefix.sh
+++ local/prefix.sh
@@ -5,50 +5,19 @@ then
   echo 'prefix.sh <compiler>'
   exit 1
 fi
-qu=$1
+q=$1
+z=$(mktemp /tmp/XXX.c)
 
-ro=$(mktemp /tmp/XXX.c)
+{
+  "$q" -v "$z" 2>&1 |
+  grep -E '^ [^ ]+$'
 
-echo 'INCLUDE'
-"$qu" -v "$ro" 2>&1 | grep -E '^ [^ ]+$' |
-while read -r si
-do
-  if [ -d "$si" ]
-  then
-    cd "$si"
-    while grep -q include <<eof
-$PWD
-eof
-    do
-      cd ..
-    done
-    pwd
-  fi
-done | sed '
-/\./d
-s/^/--prefix /
-' | sort -u
-
-echo 'LIB'
-"$qu" '-###' "$ro" 2>&1 | sed '
-/LIBRARY_PATH=/!d
-s///
-y/:/\n/
-' |
-while read -r ta
-do
-  if [ -d "$ta" ]
-  then
-    cd "$ta"
-    while grep -q lib <<eof
-$PWD
-eof
-    do
-      cd ..
-    done
-    pwd
-  fi
-done | sed '
-/\./d
-s/^/--prefix /
-' | sort -u
+  "$q" -### "$z" 2>&1 |
+  sed '
+  /LIBRARY_PATH=/!d
+  s///
+  y/:/\n/
+  '
+} |
+xargs readlink -e |
+sort -u
