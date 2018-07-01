9e37e8c82b7ef053eeb320b0b83a91162a08993e
Steven Penny <svnpenn@gmail.com>
2018-07-01 13:26:12 -0500

    qu=$(mktemp XXX.mp4)

--- local/multimedia/mm-loop.sh
+++ local/multimedia/mm-loop.sh
@@ -4,13 +4,16 @@ then
   echo 'mm-loop.sh <image> <audio>'
   exit 1
 fi
-xr=$(mktemp XXX.mp4)
-ya=$(ffprobe -v 0 -of csv=p=0 -show_entries format=duration "$2" | tr -d '\r')
+qu=$(mktemp XXX.mp4)
+xr=$(ffprobe -v 0 -of csv=p=0 -show_entries format=duration "$2" | tr -d '\r')
 
 case $2 in
-*.m4a|*.mp3) zu=copy ;;
-*) zu=libfdk_aac ;;
+*.m4a|*.mp3)
+  zu=copy
+;;
+*)
+  zu=libfdk_aac
 esac
 
-ffmpeg -y -hide_banner -loop 1 -r 1 -i "$1" -i "$2" -t "$ya" -c:a "$zu" \
--b:a 256k -pix_fmt yuv420p -vf 'setsar,scale=-2:min(720\,trunc(ih/2)*2)' "$xr"
+ffmpeg -y -hide_banner -loop 1 -r 1 -i "$1" -i "$2" -t "$xr" -c:a "$zu" \
+-b:a 256k -pix_fmt yuv420p -vf 'setsar,scale=-2:min(720\,trunc(ih/2)*2)' "$qu"
