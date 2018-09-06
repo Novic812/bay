88df91967a6d9af9785c4f831f1fe783898ff31b
Steven Penny <svnpenn@gmail.com>
2018-09-05 22:39:22 -0500

    function q()

--- scripts/stopwatch.awk
+++ scripts/stopwatch.awk
@@ -1,12 +1,14 @@
 #!/usr/bin/awk -f
-function q() {
+function q()
+{
   getline < "/proc/uptime"
   close("/proc/uptime")
   return $0
 }
 BEGIN {
   x = q()
-  while (1) {
+  while (1)
+  {
     z = q()
     printf "%02d:%05.2f\r", (z - x) / 60, (z - x) % 60
   }
--- scripts/tex.sh
+++ scripts/tex.sh
@@ -11,14 +11,17 @@ zu=$(basename "$2" .pdf)
 
 pdflatex -halt-on-error -output-directory "$ya" -jobname "$zu" "$xr" |
 awk '
-if (index($0, "Output")) {
+if (index($0, "Output"))
+{
   print "\33[1;32m" $0 "\33[m"
 }
 else if (index($0, "!") || index($0, "Overfull") ||
-index($0, "Underfull") || index($0, "Warning")) {
+index($0, "Underfull") || index($0, "Warning"))
+{
   print "\33[1;31m" $0 "\33[m"
 }
-else {
+else
+{
   print
 }
 '
