006b98fff581410dc727630590cd09004fbb442e
Steven Penny <svnpenn@gmail.com>
2018-09-09 11:49:01 -0500

    if (ARGC != 3)

--- scripts/warn.sh
+++ scripts/warn.sh
@@ -1,7 +1,7 @@
 #!/bin/dash -e
 if [ "$#" -lt 3 ]
 then
-  echo 'synopsis: warn.sh <machine> <compiler> <infile>
+   echo 'synopsis: warn.sh <machine> <compiler> <infile>
 
 machine:
 - cygwin
@@ -16,7 +16,7 @@ packages:
 - gcc-g++
 - mingw64-x86_64-gcc-core
 - mingw64-x86_64-gcc-g++'
-  exit 1
+   exit 1
 fi
 
 z="$1 $2"
@@ -25,14 +25,14 @@ set -- -s -static -Wall -Wextra -Wconversion -pedantic "$@"
 
 case $z in
 'mingw32 g++')
-  k-trace x86_64-w64-mingw32-g++ -x c++ -std=c++14 "$@"
-;;
+   k-trace x86_64-w64-mingw32-g++ -x c++ -std=c++14 "$@"
+   ;;
 'mingw32 gcc')
-  k-trace x86_64-w64-mingw32-gcc -x c -std=c11 "$@"
-;;
+   k-trace x86_64-w64-mingw32-gcc -x c -std=c11 "$@"
+   ;;
 'cygwin g++')
-  k-trace x86_64-pc-cygwin-g++ -x c++ -std=c++14 "$@"
-;;
+   k-trace x86_64-pc-cygwin-g++ -x c++ -std=c++14 "$@"
+   ;;
 'cygwin gcc')
-  k-trace x86_64-pc-cygwin-gcc -x c -std=c11 "$@"
+   k-trace x86_64-pc-cygwin-gcc -x c -std=c11 "$@"
 esac
