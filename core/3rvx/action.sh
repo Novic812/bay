a6117948778dd1ea7fec0cde22461395b4205278
Steven Penny <svnpenn@gmail.com>
2018-09-09 11:37:09 -0500

    mkdir -p "$z"/OSD

--- core/3rvx/action.sh
+++ core/3rvx/action.sh
@@ -2,15 +2,15 @@
 q=$SYSTEMDRIVE'/Program Files (x86)/3RVX/Skins'
 z=$q'/Windows Default'
 [ -d "$q" ]
-mkdir -p -v "$z"/OSD
-ln -f -s -v "$PWD"/skin.xml "$z"
-ln -f -s -v "$PWD"/settings.xml "$APPDATA"/3RVX
+mkdir -p "$z"/OSD
+ln -f -s "$PWD"/skin.xml "$z"
+ln -f -s "$PWD"/settings.xml "$APPDATA"/3RVX
 cd "$z"/OSD
 set *.png
 if [ ! -f "$1" ]
 then
-  set https://raw.githubusercontent.com/malensek/3RVX/master/Skins
-  curl -K - <<eof
+   set https://raw.githubusercontent.com/malensek/3RVX/master/Skins
+   curl -K - <<eof
 url $1/Windows%20Default/OSD/background.png
 remote-name
 url $1/Windows%20Default/OSD/bar.png
--- core/windows/history.sh
+++ core/windows/history.sh
@@ -1,10 +1,7 @@
 #!/bin/dash -e
-
 # clear explorer and wallpaper history
-if
-  REG DELETE \
-  'HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\TypedPaths' /f
+q='hkcu\software\microsoft\windows\currentversion\explorer\'
+if reg delete "$q"'typedpaths' /f
 then
-  REG DELETE \
-  'HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers\Images' /f
+   reg delete "$q"'wallpapers\images' /f
 fi
