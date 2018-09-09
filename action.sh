a72a1f8c05f78bcda479492084a01c4cc1804381
Steven Penny <svnpenn@gmail.com>
2018-09-09 10:17:19 -0500

    echo 'synopsis: action.sh <operation>

--- action.sh
+++ action.sh
@@ -1,11 +1,11 @@
 #!/bin/dash -e
 if [ "$#" != 1 ]
 then
-  echo 'synopsis: action.sh <operation>
+   echo 'synopsis: action.sh <operation>
 operations:
-  -i: install
-  -r: remove'
-  exit 1
+   -i: install
+   -r: remove'
+   exit 1
 fi
 oe=$1
 pd=$PWD
@@ -14,24 +14,24 @@ set bash 3rvx firefox git less notepad2 windows
 
 case $oe in
 -i)
-  for each
-  do
-    cd "$pd"/core/"$each"
-    ./action.sh -i
-  done
+   for each
+   do
+      cd "$pd"/core/"$each"
+      ./action.sh -i
+   done
 
-  # scripts
-  find "$pd"/scripts "$pd"/docs '(' -name '*.sh' -o -name '*.awk' ')' \
-  -exec ln -f -s -t /usr/local/bin {} +
-;;
+   # scripts
+   find "$pd"/scripts "$pd"/docs '(' -name '*.sh' -o -name '*.awk' ')' \
+   -exec ln -f -s -t /usr/local/bin {} +
+    ;;
 -r)
-  # scripts
-  find /usr/local/bin -lname "$pd/*" -delete -print
+   # scripts
+   find /usr/local/bin -lname "$pd/*" -delete -print
 
-  # core
-  for each
-  do
-    cd "$pd"/core/"$each"
-    ./action.sh -r
-  done
+   # core
+   for each
+   do
+      cd "$pd"/core/"$each"
+      ./action.sh -r
+   done
 esac
