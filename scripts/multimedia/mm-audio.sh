b89a59f9e47414b8c7e775af939f6febcc1804fa
Steven Penny <svnpenn@gmail.com>
2018-09-09 15:44:47 -0500

    echo 'synopsis: mm-audio.sh <in format> <out format> <in f

--- scripts/multimedia/mm-audio.sh
+++ scripts/multimedia/mm-audio.sh
@@ -1,15 +1,12 @@
 #!/bin/dash -e
 if [ "$#" -lt 3 ]
 then
-  cat <<'eof'
-SYNOPSIS
-  mm-audio.sh <in format> <out format> <in file>...
+   echo 'synopsis: mm-audio.sh <in format> <out format> <in file>...
 
-FORMAT
-  flac
-  m4a
-  mp3
-eof
+format:
+- flac
+- m4a
+- mp3'
   exit 1
 fi
 
@@ -19,6 +16,6 @@ shift 2
 
 for xr
 do
-  zu=$(basename "$xr" "$qu")
-  k-trace ffmpeg -v warning -stats -i "$xr" -vn -b:a 256k "$zu$wh"
+   zu=$(basename "$xr" "$qu")
+   k-trace ffmpeg -v warning -stats -i "$xr" -vn -b:a 256k "$zu$wh"
 done
